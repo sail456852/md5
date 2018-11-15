d18d6e3699b1ddcdf3579d2d39b43dc0d560ff79
sail456852 <sail456852@hotmail.com>
2018-11-15 17:00:20 +0800

    md5

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -11,16 +11,44 @@
 
 /* global md5 */
 
-;(function () {
+; (function () {
   'use strict'
 
-  var input = document.getElementById('input')
+
   document.getElementById('calculate').addEventListener(
     'click',
     function (event) {
+
+      var uniqueId = "00089183"
+      var key = document.getElementById('input').value
+      var did = "iPhone 6-1542178492489"
+      // var rTime = new Date().getTime()
+      var rTime = "14:30"
+      console.log(rTime)
+      var input = '' + key + did + rTime
       event.preventDefault()
-      document.getElementById('result').value = md5(input.value)
+      var token = document.getElementById('result').value = md5(input)
+
+      // const Http = new XMLHttpRequest();
+      // const url = 'http://127.0.0.1:9999/ditui/app/performance/myTeam.do';
+      // Http.open("GET", url);
+      // Http.setRequestHeader('uniqueId', uniqueId)
+      // Http.setRequestHeader("did", did)
+      // Http.setRequestHeader("rTime", rTime)
+      // Http.setRequestHeader("token", token) // add header then CORS rule violate
+      // Http.send();
+      // Http.onreadystatechange = (e) => {
+      //   console.log(Http.responseText)
+      // }
+
+      $.ajax({
+        url: "http://127.0.0.1:9999/ditui/app/performance/myTeam.do", success: function (result) {
+          // $("#input").html(result);
+          console.log(result);
+        }
+      });
+
     }
   )
-  input.value = '日本'
+  input.value = 'ce8e30d9-f2d1-4ba0-b174-45a661caaa84'
 }())
